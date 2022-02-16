3afd440b309ca6d95b4aa6e6a5d9f54a12ec045e
lorena_olay <olaylorena2002@gmail.com>
2022-02-16 14:44:27 +0100

    otravez

--- src/main/java/ut05estructuradealmacenamiento/matrices/ej14/MatrizEnteros.java
+++ src/main/java/ut05estructuradealmacenamiento/matrices/ej14/MatrizEnteros.java
@@ -1,2 +1,34 @@
-package ut05estructuradealmacenamiento.matrices.ej14;public class MatrizEnteros {
+package ut05estructuradealmacenamiento.matrices.ej14;
+
+import java.util.Random;
+
+public class MatrizEnteros {
+    /*Escribe un programa que dada una matriz de 3x3 enteros y realice las siguientes funciones:
+    Rellenar aleatoriamente todas las casillas de la matriz con números enteros entre 1 y 100
+    Calcular la media de todos los valores
+    Calcular el valor mínimo y el máximo de todos los valores
+    */
+    public static void main(String[] args) {
+        Random rand = new Random(100);
+        final int FILAS = 3;
+        final int COLUMNAS = 3;
+        int[][] numero = new int[FILAS][COLUMNAS];
+
+        //Carga las numero en la matriz
+        for (int f = 0; f < FILAS; f++) {
+            for (int c = 0; c < COLUMNAS; c++) {
+                numero[f][c] = letraQueToca;
+                letraQueToca++;
+            }
+        }
+
+        //Mostrar en pantalla la matriz
+        for (int f = 0; f < FILAS; f++) {
+            for (int c = 0; c < COLUMNAS; c++) {
+                System.out.print(numero[f][c] + " ");
+            }
+            System.out.println();
+
+        }
+    }
 }
