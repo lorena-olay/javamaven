a235490b04ed8f531cd75179fb4455a60a69aff6
lorena_olay <olaylorena2002@gmail.com>
2022-02-23 14:46:34 +0100

    DSA

--- src/main/java/ut05estructuradealmacenamiento/setsortedset/Ejercito.java
+++ src/main/java/ut05estructuradealmacenamiento/setsortedset/Ejercito.java
@@ -1,2 +1,28 @@
-package ut05estructuradealmacenamiento.setsortedset;public class Ejercito {
+package ut05estructuradealmacenamiento.setsortedset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ejercito {
+    /*
+PARTE A
+Aplica lo estudiado para realizar una nueva clase llamada Ejercito, que contenga un número de soldados N. El constructor debe inicializar
+la estructura SET (no hay soldados duplicados y el orden no importa). Debe haber métodos para:
+1. Saber el número de soldados que hay.
+2. Alistar a un solado.
+3. Saber si el ejército no tiene soldados.
+4. Saber si un soldado está en el ejército.
+4. Sacar todos los soldados, en forma de ArrayList.
+5. Desmatricular a un soldado del ejército.
+Crea una clase de prueba para tu clase Ejército, donde se hagan uso de los métodos anteriores.
+*/
+    public static void main(String[] args) {
+        Set<Soldado> ejercito = new HashSet<>();
+        ejercito.add(new Soldado("00000", "Nombre", "Apellido", "Apellido", 1));
+        ejercito.add(new Soldado("00000", "Nombre", "Apellido", "Apellido", 1));
+        ejercito.add(new Soldado("00001", "Nombre", "Apellido", "Apellido", 1));
+
+
+    }
+
 }
--- src/main/java/ut05estructuradealmacenamiento/setsortedset/Soldado.java
+++ src/main/java/ut05estructuradealmacenamiento/setsortedset/Soldado.java
@@ -1,30 +1,90 @@
 package ut05estructuradealmacenamiento.setsortedset;
 
+import java.util.Objects;
+
 public class Soldado {
+    private String nif, nombre, apellido1, apellido2;
+    private int edad;
+
+    public Soldado() {
+    }
+
+    public Soldado(String nif, String nombre, String apellido1, String apellido2, int edad) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Soldado{" + "nif='" + nif + '\'' + ", nombre='" + nombre + '\'' + ", apellido1='" + apellido1 + '\'' + ", apellido2='" + apellido2 + '\'' + ", edad=" + edad + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Soldado)) return false;
+        Soldado soldado = (Soldado) o;
+        return getNif().equals(soldado.getNif());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNif());
+    }
+
     /*
 Busca en Internet para qué se utiliza la interface <<Comparable>> y cómo se implementa.
 Crea la clase Soldado (nif, nombre, apellido1, apellido2 y edad) con constructores, getters, setters, toString y método equals.
 Se entienden que dos soldados son iguales si su nif es igual.
+*/
 
-
-PARTE A
-Aplica lo estudiado para realizar una nueva clase llamada Ejercito, que contenga un número de soldados N. El constructor debe inicializar la estructura SET (no hay soldados duplicados y el orden no importa). Debe haber métodos para:
-1. Saber el número de soldados que hay.
-2. Alistar a un solado.
-3. Saber si el ejército no tiene soldados.
-4. Saber si un soldado está en el ejército.
-4. Sacar todos los soldados, en forma de ArrayList.
-5. Desmatricular a un soldado del ejército.
-
-Crea una clase de prueba para tu clase Ejército, donde se hagan uso de los métodos anteriores.
-
-
+/*
 PARTE B
 Crea una clase EjercitoOrdenado donde haya la misma funcionalidad pero hay que tener en cuenta que en este caso no hay duplicados y los soldados están ordenados según su nif.
-
-
 Crea una clase de prueba para tu clase EjercitoOrdenado, donde se haga uso de los métodos anteriores.
-
 Sube el proyecto una vez finalizada la tarea.
-    * */
+*/
 }
