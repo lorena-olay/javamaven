9b057329f7afb7e00ff6c13659eb9e11c8da89df
lorena-olay <83174612+lorena-olay@users.noreply.github.com>
2022-03-10 17:31:23 +0100

    prueba

--- src/main/java/ut05estructuradealmacenamiento/emparejandocalcetines/emparejandocalcetines/Tallas.java
+++ src/main/java/ut05estructuradealmacenamiento/emparejandocalcetines/emparejandocalcetines/Tallas.java
@@ -1,26 +1,24 @@
 package ut05estructuradealmacenamiento.emparejandocalcetines;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
-public enum Colores {
-    NEGRO,
-    GRISCLARO,
-    GRISOSCURO,
+public enum Tallas {
+        CUARENTA,
+        CUARENTAYUNO,
+        CUARENTAYDOS,
+        CUARENTAYTRES,
     ;
 
-    private static final List<Colores> VALUES = List.of(values());
+
+    private static final List<Tallas> VALUES = List.of(values());
     private static final int SIZE = VALUES.size();
     private static final Random RANDOM = new Random();
 
-    public static Colores randomColores()  {
+    public static Tallas randomTallas()  {
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
-    Colores() {
+
+    Tallas() {
     }
-
-
 }
-
