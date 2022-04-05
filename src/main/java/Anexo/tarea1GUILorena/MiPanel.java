cb036dc95515f79e5bf6b81546950f82068aa527
lorena_olay <olaylorena2002@gmail.com>
2022-04-05 12:53:24 +0200

    interfaces tarea1

--- src/main/java/Anexo/tarea1GUILorena/MiPanel.java
+++ src/main/java/Anexo/tarea1GUILorena/MiPanel.java
@@ -1,2 +1,34 @@
-package Anexo.tarea1GUILorena;public class MiPanel {
+package Anexo.tarea1GUILorena;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MiPanel extends JPanel {
+
+    // Añadimos un componente de tipo etiqueta (JLabel)
+    private JLabel etiqueta;
+
+    // Constructor pasando un color, que llama al método initComponents(),
+    public MiPanel(Color color){
+        initComponents();
+        cambiarColorFondo(color);
+    }
+
+    // Método que inicializa los elementos gráficos del formulario
+    private void initComponents(){
+        // Construimos la etiqueta con el texto que queremos que muestre
+        etiqueta = new JLabel("Etiqueta de MiPanel");
+        // Añade la etiqueta al contenedor MiPanel (al JPanel)
+        this.add(etiqueta);
+    }
+
+    private void cambiarColorFondo(Color color){
+        // Establece el color de fondo del panel
+        this.setBackground(color);
+    }
+
+    // Método getter
+    public JLabel getEtiqueta() {
+        return etiqueta;
+    }
 }
--- src/main/java/Anexo/tarea1GUILorena/Test.java
+++ src/main/java/Anexo/tarea1GUILorena/Test.java
@@ -1,4 +1,27 @@
 package Anexo.tarea1GUILorena;
 
+import javax.swing.*;
+
 public class Test {
+    //a . El color de los paneles sea diferente al del ejemplo.
+    //b . El nombre de la ventana sea tu nombre y apellidos.
+    //c . La ventana no sea redimensionable.
+    //d . El texto que se muestre en cada uno de los cinco paneles de
+    // color sea diferente (pista: crea los paneles de forma independiente,
+    // accede a su etiqueta y cambia el texto de la misma usando el método
+    // setText de la etiqueta).
+    public static void main(String[] args) {
+        // Construimos la ventana
+        JFrame ventanaPrincipal = new JFrame("Lorena Olay Unciano");
+        // Establecemos posición y tamaño
+        //ventanaPrincipal.setBounds(250, 250, 800, 600);
+        ventanaPrincipal.setBounds(250, 250, 800, 600);
+        ventanaPrincipal.setSize(800, 600);
+        ventanaPrincipal.setLocationRelativeTo(null);
+
+        // Hacemos visible la ventana
+        ventanaPrincipal.setVisible(true);
+        // Acción por defecto al pulsar el botón de cierre
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
