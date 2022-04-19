98253b178db18b879ebf4940b9bf92a8096e84f0
lorena-olay <83174612+lorena-olay@users.noreply.github.com>
2022-04-19 18:00:13 +0200

    Escritura de fichero por teclado

--- src/main/java/EjerciciosUT07Ficheros/ej02/EscribirFicheroLinea.java
+++ src/main/java/EjerciciosUT07Ficheros/ej02/EscribirFicheroLinea.java
@@ -11,16 +11,17 @@ public class EscribirFicheroLinea {
         Scanner entradaTeclado = new Scanner(System.in);
         // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
         String idfichero = "teclado.txt";
+        String escrito;
         // Array a escribir
-        System.out.printf("Escribe: ");
-        String escrito = entradaTeclado.nextLine();
         // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
         // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
         try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))) {
             // Usamos metodo write() para escribir en el buffer
             do {
+                System.out.printf("Escribe: ");
+                escrito = entradaTeclado.nextLine();
                 flujo.write(escrito + "\n");
-            }while(escrito!="EOF");
+            }while(!(escrito.equals("EOF")));
 
 
             // Metodo newLine() añade línea en blanco
