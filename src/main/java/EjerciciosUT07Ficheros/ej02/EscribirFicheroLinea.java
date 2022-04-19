a64758ddbd052ef815bee1de5b8b28be352ce2e7
lorena_olay <olaylorena2002@gmail.com>
2022-04-19 14:44:37 +0200

    por hacer

--- src/main/java/EjerciciosUT07Ficheros/ej02/EscribirFicheroLinea.java
+++ src/main/java/EjerciciosUT07Ficheros/ej02/EscribirFicheroLinea.java
@@ -3,36 +3,33 @@ package EjerciciosUT07Ficheros.ej02;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class EscribirFicheroLinea {
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner entradaTeclado = new Scanner(System.in);
         // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
-        String idfichero = "array.txt";
-
+        String idfichero = "teclado.txt";
         // Array a escribir
-        int numeros [][] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
-
-// Estructura try-with-resources. Instancia el objeto con el fichero a escribir
+        System.out.printf("Escribe: ");
+        String escrito = entradaTeclado.nextLine();
+        // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
         // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
-        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))){
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))) {
+            // Usamos metodo write() para escribir en el buffer
+            do {
+                flujo.write(escrito + "\n");
+            }while(escrito!="EOF");
 
-            for (int[] is : numeros) {
-                for (int i : is) {
-                    // Usamos metodo write() para escribir en el buffer
-                    flujo.write(i + ",");
-                }
-                // Metodo newLine() añade línea en blanco
-                flujo.newLine();
-            }
+
+            // Metodo newLine() añade línea en blanco
+            flujo.newLine();
+
             // Metodo flush() guarda cambios en disco
             flujo.flush();
-
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
     }
 }
