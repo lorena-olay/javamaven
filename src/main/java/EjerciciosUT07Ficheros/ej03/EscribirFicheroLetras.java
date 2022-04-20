0995ddcf8a167e758a694b3a0095b02523dfb85c
lorena_olay <olaylorena2002@gmail.com>
2022-04-20 14:28:08 +0200

    Escribir letras aleatorias en un fichero (controlando el número de lineas)

--- src/main/java/EjerciciosUT07Ficheros/ej03/EscribirFicheroLetras.java
+++ src/main/java/EjerciciosUT07Ficheros/ej03/EscribirFicheroLetras.java
@@ -1,35 +1,32 @@
 package EjerciciosUT07Ficheros.ej03;
 
-import org.apache.commons.lang3.RandomStringUtils;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Locale;
 
 public class EscribirFicheroLetras {
-    public static void main(String[] args)
-    {
+    public static String letraRandom(){
+        String linea = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstivwxyz";
+        char caracter = linea.charAt((int) (Math.random()*linea.length()));
+        return String.valueOf(caracter);
+    }
+    public static void main(String[] args) {
         // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
         String idfichero = "75lineas.txt";
-        //String linea = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrsty";
-        String linea;
+        final int NUMLINEAS = 71;
         int contador = 0;
-        final int limiteLineas = 75;
-        // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
-        // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
-        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))){
-            do {
-                linea = RandomStringUtils.randomAlphabetic(1).toUpperCase(Locale.ROOT);
-                if(linea.equals("g")||linea.equals("G")){
-                    flujo.write(linea+"\n");
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idfichero))) {
+            do{
+                String aux = letraRandom();
+                if (aux.equals("g")||aux.equals("G")) {
+                    flujo.write(aux + ";");
+                    flujo.newLine();
                     contador++;
                 }else {
-                    flujo.write(linea + ";");
-                    contador++;
+                    flujo.write(aux + ";");
                 }
-            }while(contador!=limiteLineas);
 
+            }while (contador!=NUMLINEAS);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
