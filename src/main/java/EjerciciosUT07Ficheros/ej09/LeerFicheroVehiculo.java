d1f7ace15cea260527d75f95cb7494dbda17ab69
lorena_olay <olaylorena2002@gmail.com>
2022-04-26 14:44:52 +0200

    por hacer

---
+++ src/main/java/EjerciciosUT07Ficheros/ej09/LeerFicheroVehiculo.java
@@ -0,0 +1,30 @@
+package EjerciciosUT07Ficheros.ej09;
+
+import EjerciciosUT06.ej02.Vehiculo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LeerFicheroVehiculo {
+    public static void main(String[] args) throws FileNotFoundException {
+        //7.- Realiza un programa que lea los datos del ejercicio 4. Para ello creará una lista de objetos de
+        // tipo Vehículo. El programa irá almacenando en la lista los objetos leídos desde el archivo de texto
+        // “vehículos.txt”. Una vez cargados todos los datos en la lista, ordena los vehículos por Marca y muestra
+        // el resultado por consola.
+        String idFichero = "vehículos.txt";
+        String linea;
+        ArrayList<Vehiculo> listavehiculo = new ArrayList<>();
+        System.out.println("Leyendo el fichero: " + idFichero);
+        try (Scanner datosFichero = new Scanner(new FileReader(idFichero))) {
+            while (datosFichero.hasNextLine()) {
+                linea = datosFichero.nextLine();
+                System.out.println(linea);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
