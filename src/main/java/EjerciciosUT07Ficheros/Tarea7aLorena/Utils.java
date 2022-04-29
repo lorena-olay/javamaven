591adb0f9380a539ef48f42d9bfbfbc4b4b7b4d2
lorena_olay <olaylorena2002@gmail.com>
2022-04-29 14:41:39 +0200

    hacerlo con la clase stream

--- src/main/java/EjerciciosUT07Ficheros/Tarea7aLorena/Utils.java
+++ src/main/java/EjerciciosUT07Ficheros/Tarea7aLorena/Utils.java
@@ -2,26 +2,19 @@ package EjerciciosUT07Ficheros.Tarea7aLorena;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Utils {
     //A partir de una lista de empleados y un nombre, indique si hay algún empleado con ese nombre.
-    public void EmpleadoNombreExiste(ArrayList<Empleado> lista, String nombre) {
-        if (lista.contains(nombre)) {
-            System.out.println("Existen empleados con el nombre: " + nombre);
-        }
+    public boolean EmpleadoNombreExiste(ArrayList<Empleado> lista, String nombre) {
+        Stream<ArrayList<Empleado>> s = Stream.of(lista);
+        return s.anyMatch(empleados -> lista.contains(nombre)); //???
     }
 
     //A partir de una lista de empleados y un nombre de departamento, indique el número de empleados Coordinadores de ese departamento.
-    public void EmpleadosDepartmentoNumCoordinadores(ArrayList<Empleado> lista, String departamentoNombre) {
-        int numero=0;
-        for (Empleado e : lista) {
-            if (e.getPuesto().equals(departamentoNombre)) {
-                if (e.isCoordinador().equals("Sí")) {
-                    numero++;
-                }
-            }
-        }
-        System.out.println("El número de empleados Coordinadores de "+departamentoNombre+" es "+numero);
+    public int EmpleadosDepartmentoNumCoordinadores(ArrayList<Empleado> lista, String departamentoNombre) {
+        Stream<ArrayList<Empleado>> s = Stream.of(lista);
+        return s.;
     }
     //A partir de una lista de empleados y una letra del NIF (char), obtener una nueva lista ordenada alfabéticamente
     // SOLO con los apellidos de los empleados cuyo NIF contenga esa letra.
