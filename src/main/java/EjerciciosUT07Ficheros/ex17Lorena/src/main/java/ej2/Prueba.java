75e8a45c4b3bd9bb03f51d080add12796f268c65
lorena_olay <olaylorena2002@gmail.com>
2022-05-27 14:46:40 +0200

    EXAMEN REC7

--- src/main/java/EjerciciosUT07Ficheros/ex17Lorena/src/main/java/ej2/Prueba.java
+++ src/main/java/EjerciciosUT07Ficheros/ex17Lorena/src/main/java/ej2/Prueba.java
@@ -3,7 +3,8 @@ package ej2;
 import java.io.FileNotFoundException;
 
 public class Prueba {
+
     public static void main(String[] args) throws FileNotFoundException {
-        Lectura.leer("./escaleraColor.txt");
+        ej2.Lectura.leer("./escaleraColor.txt");
     }
 }
