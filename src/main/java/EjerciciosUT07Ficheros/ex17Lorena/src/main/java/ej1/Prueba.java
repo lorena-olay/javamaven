e5e8884b52ca189a6f30a5999463b09c8c7e440e
lorenay02 <olaylorena2002@gmail.com>
2022-06-08 14:21:08 +0200

    .

--- src/main/java/EjerciciosUT07Ficheros/ex17Lorena/src/main/java/ej1/Prueba.java
+++ src/main/java/EjerciciosUT07Ficheros/ex17Lorena/src/main/java/ej1/Prueba.java
@@ -1,8 +1,9 @@
 package ej1;
 
-public class Prueba {
+public class Prueba {/*
     public static void main(String[] args) {
        Baraja baraja = new Baraja();
        Baraja.crearCartas().forEach(System.out::println);
     }
+    */
 }
